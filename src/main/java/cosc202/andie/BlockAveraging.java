8b220edcd065f4a95363ba692cd518c316c2abb4
godnm <godnm@bradyn_laptop>
2025-04-14 12:09:13 +1200

    Block averaging done, needs more testing comments and to be integrated into Andie.java

--- src/main/java/cosc202/andie/BlockAveraging.java
+++ src/main/java/cosc202/andie/BlockAveraging.java
@@ -1,101 +1,104 @@
-//package cosc202.andie;
-//
-//import java.awt.image.*;
-//
-///**
-// *
-// * @author Bradyn Salmon
-// */
-//public class BlockAveraging implements ImageOperation, java.io.Serializable {
-//
-//    private int xBA;
-//    private int yBA;
-//    
-//    public static void blockAverage(int x, int y, BufferedImage input, int[] blockArr){
-//    
-//        for blockArr{
-//            
-//            int a = (argb & 0xFF000000) >>> 24;
-//            int r = (argb & 0x00FF0000) >> 16;
-//            int g = (argb & 0x0000FF00) >> 8;
-//            int b = (argb & 0x000000FF);
-//            blockArr[i];
-//            rtotal++;
-//            gtotal++;
-//            btotal++;
-//            
-//        
-//        }
-//        rAve = rtotal/blockArr.length;
-//        gAve = gtotal/blockArr.length;
-//        bAve = btotal/blockArr.length;
-//        
-//        for yBA{
-//        
-//            for xBA{
-//            
-//                XiYiR = rAve;
-//                XiYiG = gAve;
-//                XiYiB = bAve;
-//            
-//            }
-//        
-//        }
-//    
-//    }
-//
-//    public BlockAveraging(int x, int y) {
-//
-//        this.xBA = x;
-//        this.yBA = y;
-//
-//    }
-//
-//    /**
-//     * <p>
-//     * Apply the block averaging to image.
-//     * </p>
-//     *
-//     * <p>
-//     * This method iterates through each pixel of the input image, applies the
-//     * brightness and contrast adjustments to the rgb color channels, and
-//     * returns the adjusted image.
-//     * </p>
-//     *
-//     * @param input The image to be blovk averaged
-//     * @return The image with block averaging applied
-//     */
-//    @Override
-//    public BufferedImage apply(BufferedImage input) {
-//
-//        for (int y = 1; y < input.getHeight(); y = y + yBA) {
-//
-//            for (int x = 1; x < input.getWidth(); x = x + xBA) {
-//                
-//                int [] blockArr = new int[xBA*yBA];
-//                for (int i = 0; i < yBA; i++) {
-//
-//                    for (int j = 0; j < xBA; j++) {
-//
-//                        int argb = input.getRGB(x + j, y + i);
-//                        /* >> sets shifted-in bits to match the sign (high order) bit
-//                        * >>> sets shifted-in bits to zero always
-//                         */
-//                        int a = (argb & 0xFF000000) >>> 24;
-//                        int r = (argb & 0x00FF0000) >> 16;
-//                        int g = (argb & 0x0000FF00) >> 8;
-//                        int b = (argb & 0x000000FF);
-//
-//                        argb = (a << 24) | ((int) newR << 16) | ((int) newG << 8) | (int) newB;
-//                        input.setRGB(x, y, argb);
-//
-//                    }
-//
-//                }
-//
-//            }
-//
-//        }
-//        return input;
-//    }
-//}
+package cosc202.andie;
+
+import java.awt.image.*;
+
+/**
+ *
+ * @author Bradyn Salmon
+ */
+public class BlockAveraging implements ImageOperation, java.io.Serializable {
+
+    private int xBA;
+    private int yBA;
+
+    public BlockAveraging(int x, int y) {
+
+        this.xBA = x;
+        this.yBA = y;
+
+    }
+
+    /**
+     * <p>
+     * Apply the block averaging to image.
+     * </p>
+     *
+     * <p>
+     * This method iterates through each block, the size of which is controlled
+     * by the user, and takes their RGB values and averages them to give a block
+     * pattern look to the image.
+     * </p>
+     *
+     * @param input The image to be block averaged
+     * @return The image with block averaging applied
+     */
+    @Override
+    public BufferedImage apply(BufferedImage input) {
+
+        for (int y = 0; y < input.getHeight(); y += yBA) {
+
+            for (int x = 0; x < input.getWidth(); x += xBA) {
+
+                long totalR = 0;
+                long totalG = 0;
+                long totalB = 0;
+                int pixelCount = 0;
+
+                for (int blockY = 0; blockY < yBA; blockY++) {
+
+                    for (int blockX = 0; blockX < xBA; blockX++) {
+
+                        int currentX = x + blockX;
+                        int currentY = y + blockY;
+
+                        if (currentX < input.getWidth() && currentY < input.getHeight()) {
+
+                            int argb = input.getRGB(currentX, currentY);
+                            /* >> sets shifted-in bits to match the sign (high order) bit
+                             * >>> sets shifted-in bits to zero always
+                             */
+                            int a = (argb & 0xFF000000) >>> 24;
+                            int r = (argb & 0x00FF0000) >> 16;
+                            int g = (argb & 0x0000FF00) >> 8;
+                            int b = (argb & 0x000000FF);
+
+                            totalR += r;
+                            totalG += g;
+                            totalB += b;
+                            pixelCount++;
+                        }
+
+                    }
+
+                }
+                int avgR = (int) (totalR / pixelCount);
+                int avgG = (int) (totalG / pixelCount);
+                int avgB = (int) (totalB / pixelCount);
+
+                for (int blockY = 0; blockY < yBA; blockY++) {
+
+                    for (int blockX = 0; blockX < xBA; blockX++) {
+
+                        int currentX = x + blockX;
+                        int currentY = y + blockY;
+
+                        if (currentX < input.getWidth() && currentY < input.getHeight()) {
+
+                            int argb = input.getRGB(currentX, currentY);
+                            /* >> sets shifted-in bits to match the sign (high order) bit
+                             * >>> sets shifted-in bits to zero always
+                             */
+                            int a = (argb & 0xFF000000) >>> 24;
+                            argb = (a << 24) | (avgR << 16) | (avgG << 8) | avgB;
+                            input.setRGB(currentX, currentY, argb);
+
+                        }
+
+                    }
+                }
+            }
+
+        }
+        return input;
+    }
+}
