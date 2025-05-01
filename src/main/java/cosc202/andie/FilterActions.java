792898cd147d37bef123381638396cad0d945ea0
salbr927 <salbr927@host.docker.internal>
2025-05-01 16:55:38 +1200

    Accidentally deleted icon.png so had to start over lol

--- src/main/java/cosc202/andie/FilterActions.java
+++ src/main/java/cosc202/andie/FilterActions.java
@@ -1,5 +1,6 @@
 package cosc202.andie;
 
+import java.awt.GridLayout;
 import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -45,6 +46,7 @@ public class FilterActions {
         actions.add(new GaussianBlurFilterAction(bundle.getString("GAUSSIAN FILTER"), null, bundle.getString("APPLY A GAUSSIAN FILTER"), KeyEvent.VK_M));
         actions.add(new SharpenFilterAction(bundle.getString("SHARPEN FILTER"), null, bundle.getString("APPLY A SHARPEN FILTER"), KeyEvent.VK_M));
         actions.add(new MedianFilterAction(bundle.getString("MEDIAN FILTER"), null, bundle.getString("APPLY A MEDIAN FILTER"), 0));
+        actions.add(new BlockAveragingAction(bundle.getString("BLOCK FILTER"), null, bundle.getString("APPLY BLOCK FILTER"), KeyEvent.VK_B));
     }
 
     /**
@@ -340,6 +342,98 @@ public class FilterActions {
 
         }
 
+    }
+    
+        /**
+     * <p>
+     * Action to apply block averaging to image.
+     * </p>
+     *
+     * @see BlockAveraging
+     */
+    public class BlockAveragingAction extends ImageAction {
+        
+        /**
+         * <p>
+         * Create a new block-averaging action.
+         * </p>
+         *
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if
+         * null).
+         */
+        BlockAveragingAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+        
+        /**
+         * <p>
+         * Callback for when the block-averaging action is triggered.
+         * </p>
+         *
+         * <p>
+         * This method is called whenever the BlockAveragingAction is triggered.
+         * It changes creates blocks to pixelate the image.
+         * </p>
+         *
+         * @param e The event triggering this callback.
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (!target.getImage().hasImage()) {
+                JOptionPane.showMessageDialog(null, bundle.getString("PLEASE SELECT AN IMAGE."));
+                return;
+            }
+            
+            //create a JPanel to hold both spinners
+            JPanel panel = new JPanel(new GridLayout(2, 2, 5, 5)); //2 rows, 2 columns, 5 spacing
+
+            //brightness input
+            JLabel heightLabel = new JLabel(bundle.getString("BLOCK HEIGHT"));
+            SpinnerNumberModel heightModel = new SpinnerNumberModel(0, -100, 100, 1);
+            JSpinner heightSpinner = new JSpinner(heightModel);
+
+            //contrast input
+            JLabel widthLabel = new JLabel(bundle.getString("BLOCK WIDTH"));
+            SpinnerNumberModel widthModel = new SpinnerNumberModel(0, -100, 100, 1);
+            JSpinner widthSpinner = new JSpinner(widthModel);
+
+            panel.add(heightLabel);
+            panel.add(heightSpinner);
+            panel.add(widthLabel);
+            panel.add(widthSpinner);
+
+            //show the option dialog with the panel
+            int option = JOptionPane.showOptionDialog(
+                    null,
+                    panel,
+                    bundle.getString("ENTER HEIGHT AND WIDTH OF BLOCKS"),
+                    JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    null,
+                    null
+            );
+
+            int height = 0;
+            int width = 0;
+            if (option == JOptionPane.OK_OPTION) {
+                height = heightModel.getNumber().intValue();
+                width = widthModel.getNumber().intValue();
+            } else {
+                return;
+            }
+            
+            try {
+                target.getImage().apply(new BlockAveraging(width, height));
+                target.repaint();
+                target.getParent().revalidate();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, bundle.getString("PLEASE SELECT AN IMAGE."));
+            }
+        }
     }
 
 }
